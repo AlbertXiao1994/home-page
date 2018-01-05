d5821d0582213bc9f6bab2ead615b35ae31bb9fe
Administrator@XTM-01701112049 <502583304@qq.com>
2018-01-05 16:59:23 +0800

    完成个人主页

--- js/bg.js
+++ js/bg.js
@@ -1,6 +1,4 @@
 var PIC_NUM = 10
 var body = document.getElementsByTagName('body')[0]
-var path = '../images/' + Math.floor(Math.random() * PIC_NUM) + '.jpg'
-body.style.body = 'cover'
-body.style.backgoundRepeat = 'no-repeat'
-body.style.backgoundImage = 'url(D:/Github/home-page/images/0.jpg)'
+var path = './images/' + Math.floor(Math.random() * PIC_NUM) + '.jpg'
+body.style.backgroundImage = 'url(' + path + ')'
